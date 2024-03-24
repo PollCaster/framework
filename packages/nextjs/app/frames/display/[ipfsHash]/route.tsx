67efd12ec1d6dec1373673da9f1698e9c50a84d6
Yash Goyal <yashgo0018@gmail.com>
2024-03-24 09:45:43 +0530

    fixed the existing frame

--- packages/nextjs/app/frames/display/[ipfsHash]/route.tsx
+++ packages/nextjs/app/frames/display/[ipfsHash]/route.tsx
@@ -6,22 +6,20 @@ import { Button, createFrames } from "frames.js/next";
 const frames = createFrames();
 const handleRequest = frames(async (ctx: any) => {
   const ipfsHash = ctx.url.href.split("/").slice(-1)[0];
-  console.log(ipfsHash);
   const { data } = await axios.get(`https://gateway.pinata.cloud/ipfs/${ipfsHash}`);
   console.log(data);
 
+  const state = typeof ctx.message?.state === "string" ? JSON.parse(ctx.message?.state || "{}") : ctx.message?.state;
+  const oldCount = state?.["count"] || 0;
+
   return {
-    image: (
-      <div tw="w-full h-full bg-slate-700 text-white justify-center items-center">
-        {(ctx.message?.state as any)?.count ?? 0}
-      </div>
-    ),
+    image: <div tw="w-full h-full bg-slate-700 text-white justify-center flex items-center">{oldCount}</div>,
     buttons: [
       <Button key={1} action="post">
         Increment counter
       </Button>,
     ],
-    state: { count: ((ctx.message?.state as any)?.count ?? 0) + 1 },
+    state: { count: oldCount + 1 },
   };
 });
 
