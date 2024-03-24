72f578e8a883eb9c52d58066b9ac62035d9c8dfa
Yash Goyal <yashgo0018@gmail.com>
2024-03-24 09:35:43 +0530

    added frame in the display route

--- packages/nextjs/app/frames/display/[ipfsHash]/route.tsx
+++ packages/nextjs/app/frames/display/[ipfsHash]/route.tsx
@@ -1,9 +1,29 @@
-import { NextRequest, NextResponse } from "next/server";
 import axios from "axios";
 
-export const GET = async (req: NextRequest, route: { params: { ipfsHash: string } }) => {
-  const ipfsHash = route.params.ipfsHash;
-  const { data } = await axios.get(`https://gateway.pinata.cloud/ipfs/${ipfsHash}`);
+/* eslint-disable react/jsx-key */
+import { Button, createFrames } from "frames.js/next";
 
-  return NextResponse.json(data);
-};
+const frames = createFrames();
+const handleRequest = frames(async (ctx: any) => {
+  const ipfsHash = ctx.url.href.split("/").slice(-1)[0];
+  console.log(ipfsHash);
+  const { data } = await axios.get(`https://gateway.pinata.cloud/ipfs/${ipfsHash}`);
+  console.log(data);
+
+  return {
+    image: (
+      <div tw="w-full h-full bg-slate-700 text-white justify-center items-center">
+        {(ctx.message?.state as any)?.count ?? 0}
+      </div>
+    ),
+    buttons: [
+      <Button key={1} action="post">
+        Increment counter
+      </Button>,
+    ],
+    state: { count: ((ctx.message?.state as any)?.count ?? 0) + 1 },
+  };
+});
+
+export const GET = handleRequest;
+export const POST = handleRequest;
---
+++ packages/nextjs/app/frames/display/[ipfsHash]/type.d.ts
@@ -0,0 +1,1 @@
+declare module "frames.js/next";
